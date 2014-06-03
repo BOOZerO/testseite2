4e34e64601527c5a3011cf433fbe347ae847dc07
BOOZerO <henry@boozero.de>
2014-06-04 00:23:56 +0200

    Erste Einspielung der Testseite 2

---
+++ src/de/boozero/testseite2/Main.java
@@ -0,0 +1,72 @@
+package de.boozero.testseite2;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+
+/**
+ * Application Lifecycle Listener implementation class Main
+ *
+ */
+public class Main implements ServletRequestListener, ServletContextListener, ServletRequestAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public Main() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletRequestListener#requestDestroyed(ServletRequestEvent)
+     */
+    public void requestDestroyed(ServletRequestEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
+     */
+    public void attributeAdded(ServletRequestAttributeEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
+     */
+    public void attributeRemoved(ServletRequestAttributeEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
+     */
+    public void attributeReplaced(ServletRequestAttributeEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestListener#requestInitialized(ServletRequestEvent)
+     */
+    public void requestInitialized(ServletRequestEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+	
+}
---
+++ src/de/boozero/testseite2/Welcome.java
@@ -0,0 +1,52 @@
+package de.boozero.testseite2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Welcome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("### doGet");
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head><title>Hello World Servlet</title></head>");
+		writer.println("<body>");
+		writer.println("	<h1>Hello World from a Sevlet!</h1>");
+		writer.println("<body>");
+		writer.println("</html>");
+			
+		writer.close();	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("### doPost");
+	}
+
+}
